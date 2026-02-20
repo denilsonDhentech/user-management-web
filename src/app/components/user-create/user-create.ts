ab53ef7947aac970bd545542e45c9be1b40ae608
denilsonDhentech <denilson_contato@outlook.com>
2026-02-20 17:20:23 -0300

    feat: layout form for save users

--- src/app/components/user-create/user-create.ts
+++ src/app/components/user-create/user-create.ts
@@ -1,16 +1,42 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
+import { PasswordModule } from 'primeng/password';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-user-create',
-  imports: [CardModule, ButtonModule],
+  imports: [
+    ReactiveFormsModule,
+    CardModule,
+    InputTextModule,
+    ButtonModule,
+    PasswordModule,
+    MessageModule],
   templateUrl: './user-create.html',
   styleUrl: './user-create.scss',
 })
 export class UserCreate {
+  private fb = inject(FormBuilder);
   private router = inject(Router);
+
+  loading = signal(false);
+
+  userForm = this.fb.group({
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]]
+  });
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      this.loading.set(true);
+      console.log('Dados para o Spring Boot:', this.userForm.value);
+    }
+  }
 
   goBack() {
     this.router.navigate(['/users']);
